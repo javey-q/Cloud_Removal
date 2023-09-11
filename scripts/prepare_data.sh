6c175247eaf182acb913e5f958939528bad31e35
qiujiawei <qiujiawei@mail.nwpu.edu.cn>
2023-09-11 17:59:28 +0800

    modify prepare data

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,5 +1,5 @@
 unzip /data/train_训练集（图像+真值）.zip -d /data
-unrar x /test/test.rar # test_测试集（仅图像）-> testB
+rar x /test/test.rar # test_测试集（仅图像）-> testB
 cp ./others/train_val_list.csv /data/train
 # cut 256
 python ./scipts/data_slice.py --phase train --data_root /data/train \
@@ -12,7 +12,7 @@ python ./scipts/data_slice.py --phase train --data_root /data/train \
 # cut test
 python ./scipts/data_slice.py --phase test --data_root /test/testB \
         --data_slice /test/testB_256  --data_csv   /test/testB_256/test_list.csv \
-        --slice_size 320 --overlap_rate 0.4
+        --slice_size 256 --overlap_rate 0
 cp ./others/train_val_list_new.csv ./data/train_256
 cp ./others/train_val_list_new.csv ./data/train_320
 cp ./others/test_list.csv /test/testB_256
